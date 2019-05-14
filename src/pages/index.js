6d901d49e9c9e28c047b61589a6e5338b07f457e
Sung Kyu Hong <sungkyuhong93@gmail.com>
2019-05-13 22:53:01 -0400

    index syntax changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,19 @@ import { Link } from "gatsby";
 
 import logo from "../images/Supsolution_Logo.png";
 
-import circle1 from "../images/Circle1.png";
-
 // const activeStyles = {
 //   backgroundImage: `url(${circle1})`,
 //   backgroundSize: "100% 100%",
 // }
 
-const IndexPage = () => (
-  <div className="init-logo">
-    <Link to="/about/">
-      <img className="init-logo-img fade-in" src={logo} alt="Logo" />
-    </Link>
-  </div>
-);
+const IndexPage = () => {
+  return (
+    <div className="init-logo">
+      <Link to="/about/">
+        <img className="init-logo-img fade-in" src={logo} alt="Logo" />
+      </Link>
+    </div>
+  );
+};
 
 export default IndexPage;
